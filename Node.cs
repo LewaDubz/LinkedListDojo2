f87d7412ceb1ebe1b831ebf915f7ba5ed9c3b03e
LewaDubz <lestrider@gmail.com>
2017-07-04 11:51:48 +0200

    Initial commit

---
+++ Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListDojo
+{
+    class Node
+    {
+        public int data;
+        public int node;
+        public Node next;
+
+        public Node(int data, int node, Node next)
+        {
+            this.data = data;
+            this.node = node;
+            this.next = next;
+        }
+
+        public Node Next()
+        {
+            return next;
+        }
+    }
+
+
+}
